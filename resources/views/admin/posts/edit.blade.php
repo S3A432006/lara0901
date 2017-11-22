6e2bb4df7499c2f5d30be7e737b1d5742a2db7f6
Jia Yi Lai <anniuu7134@gmail.com>
2017-11-22 12:24:34 +0800

    修改 edit.blade.php表單

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -6,6 +6,30 @@
 <!-- Page Heading -->
 <div class="row">
     <div class="col-lg-12">
+        <form action="/admin/posts/{{$post->id}}" method="POST" role="form">
+            {{ csrf_field() }}
+            {{ method_field('PATCH') }}
+
+            <div class="form-group">
+                <label>標題：</label>
+                <input name="title" class="form-control" placeholder="請輸入文章標題" value="{{$post->title}}">
+            </div>
+            <div class="form-group">
+                <label>內容：</label>
+                <textarea name="content" class="form-control" rows="10">{{$post->content}}</textarea>
+            </div>
+            <div class="form-group">
+                <label>精選？</label>
+                <select name="is_feature" class="form-control" >
+                    <option value="0" {{ $post->is_feature?'':'SELECTED' }}>否</option>
+                    <option value="1" {{ $post->is_feature?'SELECTED':'' }}>是</option>
+                </select>
+            </div>
+            <div class="text-right">
+                <button type="submit" class="btn btn-success">更新</button>
+            </div>
+        </form>
+
         <h1 class="page-header">
             編輯文章 <small>編輯文章內容</small>
         </h1>
